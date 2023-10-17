a72477f48546a32144859737888265e77bc285bb
SMU2023 <samuel.ortega@estudante.ifms.edu.br>
2023-10-17 11:33:50 -0400

    contato - navegação

--- src/main.tsx
+++ src/main.tsx
@@ -1,91 +1,114 @@
-import React from 'react'
-import './main.css'
-import Header from './componentes/header/Header'
-import Footer from './componentes/footer/Footer'
-import Main from './componentes/main/Main'
-import * as ReactDOM from "react-dom/client";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './componentes/header/Header';
+import Footer from './componentes/footer/Footer';
+import Main from './componentes/main/Main';
+import './main.css';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: 
+const ContatoForm = () => {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [email, setEmail] = useState('');
+  const [orcamento, setOrcamento] = useState('');
+
+  return (
     <>
-    <Header/>
-    <Main/>
-    <Footer/>
-    </>,
-
-  },
-
-  
-  {
-    path: "/sobre",
-    element:  
-
-    <>
-    <Header/>
-    <div className="content-main">Página Sobre!</div>
-    <Footer/>
-    </>,
-
-  },
-
-  {
-    path: "/noticias",
-    element: 
-    <>
-    <Header/>
-    <div className="content-main">Noticias</div>
-    <Footer/>
-    </>,
-  },
-
-  {
-    path: "/contato",
-    element:
-     <>
-    <Header/>
-   
-    <div className="container">
-        <h1>Formulário de Contato</h1> 
+      <Header />
+      <div className="container">
+        <h1>Formulário de Contato</h1>
         <form>
           <div>
-            <input type="text" placeholder='Nome'></input>
+            <input
+              type="text"
+              placeholder="Nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+            <p>Nome: {nome}</p>
           </div>
 
           <div>
-            <input type="text" placeholder='Sobrenome'></input>
+            <input
+              type="text"
+              placeholder="Sobrenome"
+              value={sobrenome}
+              onChange={(e) => setSobrenome(e.target.value)}
+            />
+            <p>Sobrenome: {sobrenome}</p>
           </div>
 
           <div>
-            <input type="text" placeholder='Email'></input>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p>Email: {email}</p>
           </div>
 
           <div>
-            <input type="number" placeholder='Insira o orçamento total'></input>
+            <input
+              type="number"
+              placeholder="Insira o orçamento total"
+              value={orcamento}
+              onChange={(e) => setOrcamento(e.target.value)}
+            />
+            <p>Orçamento Total: {orcamento}</p>
           </div>
-        
+
           <div>
             <input type="submit" value="Criar projeto" />
           </div>
         </form>
-      
-    </div>
-    <Footer/>
-    </>,
-  },
+      </div>
+      <Footer />
+    </>
+  );
+};
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/sobre',
+    element: (
+      <>
+        <Header />
+        <div className="content-main">Página Sobre!</div>
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/noticias',
+    element: (
+      <>
+        <Header />
+        <div className="content-main">Noticias</div>
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/contato',
+    element: <ContatoForm />,
+  },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
